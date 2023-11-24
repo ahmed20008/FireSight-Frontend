df5b983c47f95908174d60e797f7b5460ed56daf
Ahmed <sss.ahm7@gmail.com>
2023-11-24 11:50:48 +0500

    updated side img

--- src/utils/staticImages.js
+++ src/utils/staticImages.js
@@ -1,25 +1,6 @@
 let authImages = {
   logo: require("../assets/images/logo.png"),
-  authSideImg: require("../assets/images/guest-image.png"),
-};
-let globalImages = {
-  rumorLogoWhite: require("../assets/images/rumor-logo-white.png"),
-  MyImage: require("../assets/images/team-member.png"),
-  eventImage: require("../assets/images/member-event.png"),
-  instagram: require("../assets/images/instagram.svg"),
-  instagramPink: require("../assets/images/instagram-pink.svg"),
-  snapchat: require("../assets/images/snapchat.svg"),
-  snapchatPink: require("../assets/images/snapchat-pink.svg"),
-  tiktok: require("../assets/images/tiktok.svg"),
-  tiktokPink: require("../assets/images/tiktok-pink.svg"),
-  twitter: require("../assets/images/twitter.svg"),
-  twitterPink: require("../assets/images/twitter-pink.svg"),
-  youtube: require("../assets/images/youtube.svg"),
-  youtubePink: require("../assets/images/youtube-pink.svg"),
-  sortLogo: require("../assets/images/sort.svg"),
-};
-let settingImages = {
-  chevronRight: require("../assets/images/chevron-right.svg"),
+  authSideImg: require("../assets/images/auth-side-image.jpg"),
 };
 
-export { globalImages, authImages, settingImages };
+export { authImages };
