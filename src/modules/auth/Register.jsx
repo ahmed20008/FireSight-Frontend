394c49709bbdd555a0e8639b7826893233027008
Ahmed <sss.ahm7@gmail.com>
2024-01-08 00:31:47 +0500

    added validations

--- src/modules/auth/Register.jsx
+++ src/modules/auth/Register.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import authLayout from "../../layout/AuthLayout";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "../../assets/css/auth-pages.module.css?v1.0";
-import {useNavigate} from "react-router-dom";
-import {toastrOnTopCenter} from "../../utils/toastr";
-import {signUp} from "../../api/AuthApi";
+import { useNavigate } from "react-router-dom";
+import { toastrOnTopCenter } from "../../utils/toastr";
+import { signUp } from "../../api/AuthApi";
 
 const Regsiter = () => {
   const navigate = useNavigate();
@@ -27,7 +27,7 @@ const Regsiter = () => {
     e.preventDefault();
     setProcessing(true);
 
-    signUp({...signupData})
+    signUp({ ...signupData })
       .then((response) => {
         navigate("/thank-you");
       })
@@ -47,13 +47,27 @@ const Regsiter = () => {
         <div className={styles.authContentForm}>
           <div className="row">
             <div className="col-md-6 pe-md-1">
-              <input className="mb-2" type="text" id="name" name="name" placeholder="Name" value={signupData.name} onChange={(e) => setSignupData({...signupData, name: e.target.value})} required />
+              <input className="mb-2" type="text" id="name" name="name" placeholder="Name" value={signupData.name} onChange={(e) => setSignupData({ ...signupData, name: e.target.value })} required />
             </div>
             <div className="col-md-6 ps-md-1">
-              <input className="mb-2" type="tel" id="phone" name="phone" placeholder="Phone Number" value={signupData.phone} onChange={(e) => setSignupData({...signupData, phone: e.target.value})} required />
+              <input
+                className="mb-2"
+                type="tel"
+                id="phone"
+                name="phone"
+                placeholder="Phone Number"
+                value={signupData.phone}
+                onChange={(e) => {
+                  const sanitizedValue = e.target.value.replace(/[^0-9]/g, '');
+                  setSignupData({ ...signupData, phone: sanitizedValue })
+                }}
+                maxLength={12}
+                title="Please enter only digits"
+                required
+              />
             </div>
             <div className="col-md-12">
-              <input className="mb-2" type="email" id="email" name="email" placeholder="Email" value={signupData.email} onChange={(e) => setSignupData({...signupData, email: e.target.value})} required />
+              <input className="mb-2" type="email" id="email" name="email" placeholder="Email" value={signupData.email} onChange={(e) => setSignupData({ ...signupData, email: e.target.value })} required />
             </div>
             <div className="col-md-12">
               <input
@@ -63,15 +77,43 @@ const Regsiter = () => {
                 name="address"
                 placeholder="Address"
                 value={signupData.address.address}
-                onChange={(e) => setSignupData({...signupData, address: {...signupData.address, address: e.target.value}})}
+                onChange={(e) => setSignupData({ ...signupData, address: { ...signupData.address, address: e.target.value } })}
                 required
               />
             </div>
             <div className="col-md-4 pe-md-1">
-              <input className="mb-2" type="text" id="city" name="city" placeholder="City" value={signupData.address.city} onChange={(e) => setSignupData({...signupData, address: {...signupData.address, city: e.target.value}})} required />
+              <input
+                className="mb-2"
+                type="text"
+                id="city"
+                name="city"
+                placeholder="City"
+                value={signupData.address.city}
+                onChange={(e) => {
+                  const sanitizedValue = e.target.value.replace(/[^a-zA-Z]/g, ''); // Allow only alphabets
+                  setSignupData({ ...signupData, address: { ...signupData.address, city: sanitizedValue } });
+                }}
+                title="Please enter only alphabets"
+                maxLength={20}
+                required
+              />
             </div>
             <div className="col-md-4 px-md-1">
-              <input className="mb-2" type="text" id="state" name="state" placeholder="State" value={signupData.address.state} onChange={(e) => setSignupData({...signupData, address: {...signupData.address, state: e.target.value}})} required />
+              <input
+                className="mb-2"
+                type="text"
+                id="state"
+                name="state"
+                placeholder="State"
+                value={signupData.address.state}
+                onChange={(e) => {
+                  const sanitizedValue = e.target.value.replace(/[^a-zA-Z]/g, ''); // Allow only alphabets
+                  setSignupData({ ...signupData, address: { ...signupData.address, state: sanitizedValue } });
+                }}
+                title="Please enter only alphabets"
+                required
+                maxLength={20}
+              />
             </div>
             <div className="col-md-4 ps-md-1">
               <input
@@ -81,8 +123,12 @@ const Regsiter = () => {
                 name="zipcode"
                 placeholder="Zipcode"
                 value={signupData.address.zipcode}
-                onChange={(e) => setSignupData({...signupData, address: {...signupData.address, zipcode: e.target.value}})}
+                onChange={(e) => {
+                  const sanitizedValue = e.target.value.replace(/[^0-9]/g, '');
+                  setSignupData({ ...signupData, address: { ...signupData.address, zipcode: sanitizedValue } });
+                }}
                 required
+                maxLength={5}
               />
             </div>
           </div>
